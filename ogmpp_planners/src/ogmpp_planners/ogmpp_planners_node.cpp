069fe22c4f1122b099276c13cdeb2256c8c63031
Manos Tsardoulias <etsardou@gmail.com>
2015-12-17 18:06:32 +0200

    All planners are one executable now

--- ogmpp_planners/src/ogmpp_planners/ogmpp_planners_node.cpp
+++ ogmpp_planners/src/ogmpp_planners/ogmpp_planners_node.cpp
@@ -1,11 +1,11 @@
 #include "ogmpp_planners/ogmpp_prms/ogmpp_prms.hpp"
 
 /**
- * @brief The main class of PRMs
+ * @brief The main class of planners
  */
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "ogmpp_prms_node");
+  ros::init(argc, argv, "ogmpp_planners_node");
 
   ogmpp_planners::prms::ProbabilisticRoadmaps pr;
 
